d3cdb44b67113ef9db9d134635046d8bbb744d0a
Plright <plright1@protonmail.com>
2022-06-16 20:41:06 +0300

    i already explained what happened

--- setup.py
+++ setup.py
@@ -1,30 +1,5 @@
-from setuptools import setup, find_packages
-import pathlib
-from objectionpy import _version
+from setuptools import setup
 
-here = pathlib.Path(__file__).parent.resolve()
 
-setup(
-    name="objectionpy",
-    version=_version.__version__,
-    author="Adamanti",
-    author_email="adam@sallome.com",
-    url="https://github.com/adamantii/objection.py",
-    description="A modular python library for creating objection.lol projects",
-    long_description=(here / "README.md").read_text(encoding="utf-8"),
-    long_description_content_type="text/markdown",
-    packages=["objectionpy"],
-    keywords=["objection.lol", "objection", "generator", "case", "scene"],
-    install_requires=["requests"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Topic :: Artistic Software",
-        "License :: OSI Approved :: MIT License",
-    ],
-    project_urls={
-        "Documentation": "https://adamantii.github.io/objection.py/",
-        "Bug Reports": "https://github.com/adamantii/objection.py/issues",
-        "Source": "https://github.com/adamantii/objection.py/",
-    },
-)
+if __name__ == "__main__":
+    setup()
